115313b1f3083aaca94883834a9fb07c65e49d5b
drong24 <daisyrong24@gmail.com>
2024-03-20 15:09:43 -0700

    added functions for each control item

---
+++ ascii.js
@@ -0,0 +1,51 @@
+let timer = null;
+let index = 0;
+let interval = 250;
+
+function start_timer() {
+    document.getElementById('start_button').disabled = true;
+    document.getElementById('animation_list').disabled = true;
+    document.getElementById('stop_button').disabled = false;
+    timer = setInterval(animate, interval);
+}
+
+function animate() {
+    let animationValue = document.getElementById('animation_list').value;
+    let animationArray = ANIMATIONS[animationValue].split("=====\n");
+    document.getElementById('viewer').value = animationArray[index];
+    index = (index + 1) % animationArray.length;
+}
+
+function stop_timer() {
+    clearInterval(timer);
+    timer = null;
+    index = 0;
+    let animationValue = document.getElementById('animation_list').value;
+    document.getElementById('viewer').value = ANIMATIONS[animationValue];
+    document.getElementById('start_button').disabled = false;
+    document.getElementById('animation_list').disabled = false;
+    document.getElementById('stop_button').disabled = true;
+}
+
+function change_animation() {
+    let animationValue = document.getElementById('animation_list').value;
+    document.getElementById('viewer').value = ANIMATIONS[animationValue];
+}
+
+function change_text_size() {
+    let sizeValue = document.getElementById('size_list').value;
+    document.getElementById('viewer').style.fontSize = sizeValue + "pt";
+}
+
+function change_speed() {
+    if (document.getElementById('turbo_checkbox').checked) {
+        interval = 50;
+    }
+    else {
+        interval = 250;
+    }
+    if (timer != null) {
+        clearInterval(timer);
+        timer = setInterval(animate, interval);
+    }
+}
